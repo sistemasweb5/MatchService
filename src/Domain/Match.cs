6c0e23f61857ca9de1970fb2911982992ba85e32
Diego Centella Lopez <diego.centella.60090@usalesiana.edu.bo>
2024-10-23 18:58:26 -0400

    feat: update way to handle with db

--- src/Domain/Match.cs
+++
@@ -1,9 +0,0 @@
-namespace MatchService.Domain;
-
-public class Match
-{
-    public Guid Id { get; set; }
-    public required Guid User { get; set; }
-    public required Guid Worker { get; set; }
-    public required string JobType { get; set; } 
-}
