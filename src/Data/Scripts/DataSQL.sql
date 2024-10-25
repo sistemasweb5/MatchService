08813e6bb15709b22e6a9e5c476ab220cff1f65e
Diego Centella Lopez <diego.centella.60090@usalesiana.edu.bo>
2024-10-24 23:06:43 -0400

    fix: add endline

--- src/Data/Scripts/DataSQL.sql
+++ src/Data/Scripts/DataSQL.sql
@@ -67,3 +67,4 @@ INSERT INTO client (id, localization_id ,name, emailAddress, categoryId, workSch
 INSERT INTO jobs (id, user_client_id, user_worker_id, localization_id, created_at, job_type, status, description, price) VALUES
     (gen_random_uuid(), (SELECT id FROM client WHERE name = 'John Doe'), (SELECT id FROM client WHERE name = 'Mike Brown'), (SELECT id FROM localization WHERE name = 'Location A'), NOW(), 'electrician', 'assigned', 'Job description for Job 1', 100.00),
     (gen_random_uuid(), (SELECT id FROM client WHERE name = 'Jane Smith'), (SELECT id FROM client WHERE name = 'Mike Brown'), (SELECT id FROM localization WHERE name = 'Location B'), NOW(), 'plumber', 'working', 'Job description for Job 2', 200.00);
+    
