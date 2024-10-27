1407fa5fc6b83277ce0e2f96020d2bae260dc0d9
LuchoVera1 <luis.espinoza.13875568@usalesiana.edu.bo>
2024-10-26 20:22:45 -0400

    hotfix: remove syntax error in query file

--- src/Data/Scripts/DataSql.sql
+++ src/Data/Scripts/DataSql.sql
@@ -48,9 +48,9 @@ INSERT INTO category (id, rol) VALUES
     (gen_random_uuid(), 'worker');
 
 INSERT INTO localization (id, name, geom) VALUES 
-  (gen_random_uuid(), 'Location E', ST_GeomFromText('POINT(-66.156800, -17.389500)', 4326)),
-  (gen_random_uuid(), 'Location F', ST_GeomFromText('POINT(-66.170000, -17.500000)', 4326)),
-  (gen_random_uuid(), 'Location D', ST_GeomFromText('POINT(-66.190000, -17.983333)', 4326));
+  (gen_random_uuid(), 'Location A', ST_GeomFromText('POINT(-66.156800 -17.389500)', 4326)),
+  (gen_random_uuid(), 'Location B', ST_GeomFromText('POINT(-66.170000 -17.500000)', 4326)),
+  (gen_random_uuid(), 'Location C', ST_GeomFromText('POINT(-66.190000 -17.983333)', 4326));
 
 INSERT INTO workSchedule (id, startTime, endTime) VALUES 
   ('550e8400-e29b-41d4-a716-446655440006', '08:00', '17:00'), 
@@ -59,7 +59,7 @@ INSERT INTO workSchedule (id, startTime, endTime) VALUES
   ('550e8400-e29b-41d4-a716-446655440009', '12:00', '20:00'), 
   ('550e8400-e29b-41d4-a716-446655440010', '07:00', '15:00');
 
-INSERT INTO client (id, localization_id ,name, emailAddress, categoryId, workScheduleId) VALUES
+INSERT INTO client (id, localization_id, name, emailAddress, categoryId, workScheduleId) VALUES
     (gen_random_uuid(), (SELECT id FROM localization WHERE name = 'Location A') ,'John Doe', 'john@example.com', (SELECT id FROM category WHERE rol = 'user'), '550e8400-e29b-41d4-a716-446655440006'),
     (gen_random_uuid(), (SELECT id FROM localization WHERE name = 'Location B') ,'Jane Smith', 'jane@example.com', (SELECT id FROM category WHERE rol = 'user'), '550e8400-e29b-41d4-a716-446655440006'),
     (gen_random_uuid(), (SELECT id FROM localization WHERE name = 'Location C') ,'Mike Brown', 'mike@example.com', (SELECT id FROM category WHERE rol = 'worker'), '550e8400-e29b-41d4-a716-446655440006');
