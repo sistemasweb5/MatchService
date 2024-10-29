d4154b60c2cf4ddb232059e6c3fd07aaafe83f18
SebasthianSalpozz <sebasthiansalinaspozzo@gmail.com>
2024-10-29 13:02:49 -0400

    fix: query get all

--- src/Repositories/MatchRepository.cs
+++ src/Repositories/MatchRepository.cs
@@ -14,42 +14,28 @@ public class MatchRepository : IMatchRepository
         _dbConnection = dbConnection;
     }
 
-    public async Task<IEnumerable<Client>> GetAllAsync(string jobType, double radius)
+    public async Task<IEnumerable<Client>> GetAllAsync(string idJob, double radius)
     {
         const string sql = @"
-            WITH WorkerSpecialties AS (
-                SELECT 
-                    c.id AS worker_id,
-                    c.name AS worker_name,
-                    s.name AS specialty_name,
-                    l.geom AS worker_location
-                FROM 
-                    client c
-                INNER JOIN specialty s ON c.id = s.user_client_id
-                INNER JOIN localization l ON c.localization_id = l.id
-                WHERE 
-                    c.categoryId = (SELECT id FROM category WHERE rol = 'worker')
+            WITH job_info AS (
+            SELECT j.id AS job_id, j.job_type, j.localization_id AS job_location
+            FROM jobs j
+            WHERE j.id = @idJob
+            ),
+            workers_with_specialty AS (
+                SELECT c.id AS worker_id, c.localization_id AS worker_location
+                FROM client c
+                JOIN specialty s ON c.id = s.user_client_id
+                WHERE s.name = (SELECT job_type FROM job_info)
             )
-            SELECT 
-                j.id AS job_id,
-                j.job_type,
-                j.status,
-                j.description,
-                j.price,
-                j.created_at,
-                ws.worker_id,
-                ws.worker_name,
-                ws.specialty_name,
-                ST_Distance(l.geom, ws.worker_location) AS distance_meters
-            FROM 
-                jobs j
-            INNER JOIN localization l ON j.localization_id = l.id
-            INNER JOIN WorkerSpecialties ws ON j.job_type = ws.specialty_name
-                AND ST_DWithin(l.geom, ws.worker_location, @radius)  
-            WHERE 
-                j.status IN ('not assigned')
-                AND j.job_type = @jobType;    
-        ";
+            SELECT
+                w.worker_id,
+                ST_Distance(l.geom, jl.geom) AS distance_meters
+            FROM workers_with_specialty w
+            JOIN localization l ON w.worker_location = l.id
+            JOIN localization jl ON jl.id = (SELECT job_location FROM job_info)
+            WHERE ST_DWithin(l.geom, jl.geom, @radius);    
+            ";
 
         using var connection = await _dbConnection.CreateConnectionAsync();
         return await connection.QueryAsync<Client>(sql, new { jobType, radius });
